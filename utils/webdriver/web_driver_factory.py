e7887eabbb65055f45a325188663c5e2c4be712f
ForgedCore8 <77468737+ForgedCore8@users.noreply.github.com>
2024-08-01 07:47:45 -0500

    Added edge and firefox support

--- utils/webdriver/web_driver_factory.py
+++ utils/webdriver/web_driver_factory.py
@@ -1,17 +1,35 @@
 import os
-
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 
 
 class WebDriverFactory:
     """Factory class for creating web driver instances."""
 
     @staticmethod
-    def get_webdriver():
-        """Get the web driver with the extension."""
-        chrome_options = Options()
+    def get_webdriver(browser='chrome'):
+        """Get the web driver with the specified browser and extension."""
+
         proxy_path = os.path.join(os.getcwd(), "proxy")
-        chrome_options.add_argument("--log-level=3")
-        chrome_options.add_argument(f"--load-extension={proxy_path}")
-        return webdriver.Chrome(options=chrome_options)
+
+        if browser == 'chrome':
+            chrome_options = ChromeOptions()
+            chrome_options.add_argument("--log-level=3")
+            chrome_options.add_argument(f"--load-extension={proxy_path}")
+            return webdriver.Chrome(options=chrome_options)
+
+        elif browser == 'firefox':
+            firefox_options = FirefoxOptions()
+            # Add any Firefox-specific options here
+            return webdriver.Firefox(options=firefox_options)
+
+        elif browser == 'edge':
+            edge_options = EdgeOptions()
+            edge_options.add_argument("--log-level=3")
+            edge_options.add_argument(f"--load-extension={proxy_path}")
+            return webdriver.Edge(options=edge_options)
+
+        else:
+            raise ValueError(f"Unsupported browser: {browser}")
